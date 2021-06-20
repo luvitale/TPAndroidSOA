fea9fb9476cd3e2d30a34b7ea810910e28ac279f
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-06-19 22:40:35 -0300

    Stored pattern with shared preferences

--- app/src/main/java/ar/edu/unlam/tpandroidsoa/PatternActivity.java
+++ app/src/main/java/ar/edu/unlam/tpandroidsoa/PatternActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,39 +12,33 @@ import android.widget.Toast;
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
 import com.andrognito.patternlockview.utils.PatternLockUtils;
-import ar.edu.unlam.tpandroidsoa.R;
 
 import java.util.List;
 
-import io.paperdb.Paper;
-
 public class PatternActivity extends AppCompatActivity {
 
-    String save_pattern_key = "pattern_code";
-    String final_pattern = "";
+    String finalPattern = "";
     PatternLockView mPatternLockView;
+    MySharedPreferences sharedPreferences = MySharedPreferences.getSharedPreferences(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pattern);
 
-        Paper.init(this);
-        final String save_pattern = Paper.book().read(save_pattern_key);
-
-        if (savePatternExists(save_pattern)) {
-            checkPattern(save_pattern);
+        if (sharedPreferences.patternExists()) {
+            Log.d("PatternActivity", "UnlockActivity");
+            startUnlockActivity();
         }
 
         else {
-            changePattern();
+            Log.d("PatternActivity", "ListenPattern");
+            listenPattern();
+            listenButtonToSave();
         }
     }
 
-    private void changePattern() {
-        setContentView(R.layout.activity_pattern);
-
-        // Listen pattern
+    private void listenPattern() {
         mPatternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
         mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
             @Override
@@ -54,57 +49,29 @@ public class PatternActivity extends AppCompatActivity {
 
             @Override
             public void onComplete(List<PatternLockView.Dot> pattern) {
-                final_pattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
+                finalPattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
             }
 
             @Override
             public void onCleared() {}
         });
+    }
 
-        // Save pattern
+    private void listenButtonToSave() {
         Button btnSaveUnlockPattern = (Button) findViewById(R.id.btnSaveUnlockPattern);
         btnSaveUnlockPattern.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Paper.book().write(save_pattern_key, final_pattern);
+                sharedPreferences.setPattern(finalPattern);
                 Toast.makeText(PatternActivity.this, "Patrón guardado con éxito", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(PatternActivity.this, UnlockActivity.class);
-                startActivity(intent);
+                startUnlockActivity();
             }
         });
     }
 
-    private boolean savePatternExists(final String save_pattern) {
-        return save_pattern != null && !save_pattern.equals("null");
-    }
-
-    private void checkPattern(final String save_pattern) {
-        setContentView(R.layout.activity_unlock);
-        mPatternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
-        mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
-            @Override
-            public void onStarted() {}
-
-            @Override
-            public void onProgress(List<PatternLockView.Dot> progressPattern) {}
-
-            @Override
-            public void onComplete(List<PatternLockView.Dot> pattern) {
-                final_pattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
-                if (final_pattern.equals(save_pattern)) {
-                    Toast.makeText(PatternActivity.this, "Patrón ingresado correctamente", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(PatternActivity.this, SessionActivity.class);
-                    startActivity(intent);
-                }
-
-                else {
-                    Toast.makeText(PatternActivity.this, "Patrón incorrecto", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCleared() {}
-        });
+    private void startUnlockActivity() {
+        Intent intent = new Intent(PatternActivity.this, UnlockActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/ar/edu/unlam/tpandroidsoa/UnlockActivity.java
+++ app/src/main/java/ar/edu/unlam/tpandroidsoa/UnlockActivity.java
@@ -11,40 +11,30 @@ import android.widget.Toast;
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
 import com.andrognito.patternlockview.utils.PatternLockUtils;
-import ar.edu.unlam.tpandroidsoa.R;
 
 import java.util.List;
 
-import io.paperdb.Paper;
-
 public class UnlockActivity extends AppCompatActivity {
 
-    String save_pattern_key = "pattern_code";
-    String final_pattern = "";
+    String finalPattern = "";
     PatternLockView mPatternLockView;
+    MySharedPreferences sharedPreferences = MySharedPreferences.getSharedPreferences(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unlock);
 
-        Paper.init(this);
-        final String save_pattern = Paper.book().read(save_pattern_key);
-
-        if (savePatternExists(save_pattern)) {
-            checkPattern(save_pattern);
+        if (sharedPreferences.patternExists()) {
+            checkPattern();
         }
 
         else {
-            setContentView(R.layout.activity_pattern);
+            startPatternActivity();
         }
     }
 
-    private boolean savePatternExists(final String save_pattern) {
-        return save_pattern != null && !save_pattern.equals("null");
-    }
-
-    private void checkPattern(final String save_pattern) {
+    private void checkPattern() {
         setContentView(R.layout.activity_unlock);
         mPatternLockView = (PatternLockView) findViewById(R.id.pattern_lock_view);
         mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
@@ -56,21 +46,33 @@ public class UnlockActivity extends AppCompatActivity {
 
             @Override
             public void onComplete(List<PatternLockView.Dot> pattern) {
-                final_pattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
-                if (final_pattern.equals(save_pattern)) {
+                finalPattern = PatternLockUtils.patternToString(mPatternLockView, pattern);
+
+                if (finalPattern.equals(sharedPreferences.getPattern())) {
                     Toast.makeText(UnlockActivity.this, "Patrón ingresado correctamente", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(UnlockActivity.this, SessionActivity.class);
-                    startActivity(intent);
+                    startSessionActivity();
                 }
 
                 else {
                     Toast.makeText(UnlockActivity.this, "Patrón incorrecto", Toast.LENGTH_SHORT).show();
                 }
+
+                pattern.clear();
             }
 
             @Override
             public void onCleared() {}
         });
+    }
+
+    private void startSessionActivity() {
+        Intent intent = new Intent(UnlockActivity.this, SessionActivity.class);
+        startActivity(intent);
+    }
+
+    private void startPatternActivity() {
+        Intent intent = new Intent(UnlockActivity.this, PatternActivity.class);
+        startActivity(intent);
     }
 
     @Override
